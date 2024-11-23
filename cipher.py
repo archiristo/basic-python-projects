b984fe31e060fd1deffaf0bdcedd2348dd8e67b8
iris <109429124+archiristo@users.noreply.github.com>
2024-11-23 19:53:43 +0300

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,35 @@
+#I did this for revising cipher exercise after studying from FreeCodeCamp!
+
+text = 'archiristo'
+custom_key = 'archpy'
+
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+
+        if not char.isalpha():
+            final_message += char
+        else:        
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
